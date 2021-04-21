2a3397987f1a8cc748e473f8f28963a882c473cd
kameel4 <74598708+kameel4@users.noreply.github.com>
2021-04-21 21:13:22 +0500

    Add files via upload

---
+++ CheckFunctions.py
@@ -0,0 +1,16 @@
+def check_join(id):
+    with open('info/joined.txt', 'r') as joined:
+        data = joined.readlines()
+    if id not in [x.rstrip('\n') for x in data]:
+        joined.close()
+        with open('info/joined.txt', 'a') as joined:
+            joined.write(str(id) + '\n')
+
+
+def check_teacher(id):
+    with open('info/teachers.txt', 'r') as file:
+        teachers = file.readlines()
+    if id in [x.split()[1].rstrip('\n') for x in teachers]:
+        return True
+    else:
+        return False
