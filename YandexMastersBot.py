3709e0ad37e2a63fb1a293e2c7fe63a2fd79db0b
Henry <hhunrey@yandex.ru>
2021-04-21 21:41:40 +0500

    Added 'add_teachers' function

--- YandexMastersBot.py
+++ YandexMastersBot.py
@@ -1,8 +1,6 @@
 from telegram.ext import *
 import json
-import telebot
-from pprint import pprint
-from datetime import datetime
+from CheckFunctions import *
 
 updater = Updater('1676363369:AAHYqAQ2MxbytBdBPJkvtAxx1FwqBSzoXKw', use_context=True)
 dp = updater.dispatcher
@@ -12,21 +10,14 @@ for line in joinedFile:
     joinedUsers.add(line.strip())
 joinedFile.close()
 
-bot = telebot.TeleBot('1676363369:AAHYqAQ2MxbytBdBPJkvtAxx1FwqBSzoXKw')
-
 
 def main():
-    timetable_handler = MessageHandler(Filters.text, set_timetable)
-    # start_command_handler = CommandHandler('start', checkuserCommand)
-
-    dp.add_handler(timetable_handler)
     updater.start_polling()
-
     updater.idle()
 
 
 def set_timetable(update, context):
-    if update.message.from_user.id == 988566680 or update.message.from_user.id == 641113946:
+    if check_teacher(update.message.from_user.id):
         with open('info/timetable.json', 'r', encoding='UTF-8') as file:
             data = json.load(file)
             day = context.args[0]
@@ -67,30 +58,20 @@ def bell(update, context):
     update.message.reply_text(s)
 
 
-@bot(commands=['start'])
-def botMessage(message):
-    if not str(message.chat.id) in joinedUsers:
-        joinedFile = open("info/joined.txt", 'a')
-        joinedFile.write(str(message.chat.id) + '\n')
-        joinedUsers.add(message.chat.id)
-
-
-@bot.message_handler(commands=['special'])
-def botSpecial(message):
-    for user in joinedUsers:
-        bot.send_message(user, message.text[message.text.find(' '):])
-
-
-# def checkuserCommand(bot, update):
-#     if update.message.from_user.id == "Свой ID или список ID пользователей на доступ к боту":
-#         if update.message.text == "Команда":
-#             startCommand(bot, update)
+def add_teacher(update, context):
+    password = context.args[0]
+    name = context.args[1]
+    if password == 'deadline':
+        with open('info/teachers.txt', 'a', encoding='UTF-8') as teachers:
+            teachers.write(f'{name} {update.message.from_user.id}\n')
+    else:
+        update.message.reply_text('Ты не учитель, пшел вон отсюда')
 
 
 dp.add_handler(CommandHandler("set_timetable", set_timetable))
 dp.add_handler(CommandHandler("timetable", timetable))
 dp.add_handler(CommandHandler("bell", bell))
-# dp.add_handler(start_command_handler)
+dp.add_handler(CommandHandler("add_teacher", add_teacher))
 
 if __name__ == '__main__':
     main()
